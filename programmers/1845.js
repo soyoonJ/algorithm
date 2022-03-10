099f64422a26e492f680448c9819463b747538dd
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2022-03-10 11:55:21 +0900

    1845 폰켓몬 set 사용 수정

--- programmers/1845.js
+++ programmers/1845.js
@@ -1,22 +1,25 @@
 // 폰켓몬
 // https://programmers.co.kr/learn/courses/30/lessons/1845
 
-// filter 사용 방법
-function solution (nums) {
-    // 최대 가져갈 수 있는 수
-    const max = nums.length/2
-    // 중복제거한 배열
-    const filter = nums.filter((e,i)=> nums.indexOf(e) === i).length
+// // filter 사용 방법
+// function solution (nums) {
+//     // 최대 가져갈 수 있는 수
+//     const max = nums.length/2
+//     // 중복제거한 배열
+//     const filter = nums.filter((e,i)=> nums.indexOf(e) === i).length
     
-    return filter > max ? max : filter
-}
+//     return filter > max ? max : filter
+// }
 
 // 이게 더 빠름
-// function solution(nums) {
-//     // 최대 가져갈 수 있는 폰켓몬 수
-//     const max = nums.length/2
-//     // 폰켓몬 중복제거
-//     const origin = [...new Set(nums)]
-//     // console.log(...origin)
-//     return origin.length > max ? max : origin.length
-// }
+// new Set 사용할 때는 length 말고 size 쓰면 한방에 해결 가능!
+function solution(nums) {
+    // 최대 가져갈 수 있는 폰켓몬 수
+    const max = nums.length/2
+    // 폰켓몬 중복제거
+    // const origin = [...new Set(nums)]
+    const origin = new Set(nums).size
+    // console.log(...origin)
+    // return origin.length > max ? max : origin.length
+    return origin > max ? max : origin
+}
