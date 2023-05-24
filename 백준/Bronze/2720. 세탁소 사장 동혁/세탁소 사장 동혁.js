eaee29e50cc3eac80a0cb95595ad345beb9e22c7
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2023-05-24 23:21:27 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 232 ms, Memory: 12104 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input = [];
+rl.on("line", function (line) {
+  input.push(+line.toString());
+}).on("close", function () {
+  input.shift();
+
+  const coins = [25, 10, 5, 1];
+
+  let result = [];
+  for (let change of input) {
+    let count = [];
+    for (let coin of coins) {
+      count.push(Math.floor(change / coin));
+      change %= coin;
+    }
+    result.push(console.log(count[0], count[1], count[2], count[3]));
+  }
+  console.log(result.join("\n"));
+
+  process.exit();
+});
