b1948513d8dab6545d34d2b8ab57e7ac681942e1
Soyoon Jeong <96245651+soyoonJ@users.noreply.github.com>
2023-07-19 22:30:54 +0900

    [Silver V] Title: 나이순 정렬, Time: 412 ms, Memory: 47732 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -4,13 +4,10 @@ const input = fs.readFileSync("dev/stdin").toString().split("\n");
 const N = Number(input[0]);
 const newArr = [];
 for (let i = 1; i <= N; i++) {
-  newArr.push([...input[i].split(" "), i]);
+  newArr.push(input[i].split(" "));
 }
 
-newArr.sort((a, b) => {
-  if (Number(a[0]) !== Number(b[0])) return Number(a[0]) - Number(b[0]);
-  else a[2] - b[2];
-});
+newArr.sort((a, b) => Number(a[0]) - Number(b[0]));
 
 let answer = "";
 for (let member of newArr) {
